d7391b823f2619b8985742835b3bb34be9e47500
rijalNR45 <rijal.nirajan45@gmail.com>
2024-09-27 21:51:35 +0545

    the grind continues

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-const mainContainer = document.querySelector(".main-container");
-mainContainer.addEventListener('click', () =>{
-    let a,b;
-    return a+b;
+const display = document.querySelector(".display-screen");
+display.textContent = 0;
+var firstInput = [0];
+const numberOne = document.getElementById("one");
+numberOne.addEventListener('click', () =>{
+    firstInput = [1];
+    display.textContent = 1;
 })
+const addBtn = document.getElementById("plus")
+addBtn.addEventListener('click',() => {
+    firstInput.reduce((acc,curr), () => {
+        return acc + curr;
+    },0)
+} )
