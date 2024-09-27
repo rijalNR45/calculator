c5a6105656a12fbb51be0fc61ca30283c6316497
rijalNR45 <rijal.nirajan45@gmail.com>
2024-09-27 21:16:23 +0545

    writing spagetti code to just make it work, will gradually made the code better

--- script.js
+++ script.js
@@ -1,5 +1,121 @@
-const mainContainer = document.querySelector(".main-container");
-mainContainer.addEventListener('click', () =>{
-    let a,b;
-    return a+b;
+const numberOne = document.getElementById("one");
+const numberTwo = document.getElementById("two");
+const numberThree = document.getElementById("three");
+const numberFour = document.getElementById("four");
+const numberFive = document.getElementById("five");
+const numberSix = document.getElementById("six");
+const numberSeven = document.getElementById("seven");
+const numberEight = document.getElementById("eight");
+const numberNine = document.getElementById("nine");
+const numberZero = document.getElementById("zero");
+const addBtn = document.getElementById("plus");
+const subtractBtn = document.getElementById("minus");
+const multiplyBtn = document.getElementById("multiply");
+const divideBtn = document.getElementById("divide");
+
+let firstInput = 0;
+numberOne.addEventListener('click', () => {
+    console.log("clicked")
+    firstInput = 1;
+    displayingInput();
 })
+numberTwo.addEventListener('click', () => {
+    console.log("clicked")
+    firstInput = 2;
+    displayingInput();
+})
+numberOne.addEventListener('click', () => {
+    console.log("clicked")
+    firstInput = 1;
+    displayingInput();
+})
+numberOne.addEventListener('click', () => {
+    console.log("clicked")
+    firstInput = 1;
+    displayingInput();
+})
+numberOne.addEventListener('click', () => {
+    console.log("clicked")
+    firstInput = 1;
+    displayingInput();
+})
+numberOne.addEventListener('click', () => {
+    console.log("clicked")
+    firstInput = 1;
+    displayingInput();
+})
+numberOne.addEventListener('click', () => {
+    console.log("clicked")
+    firstInput = 1;
+    displayingInput();
+})
+numberOne.addEventListener('click', () => {
+    console.log("clicked")
+    firstInput = 1;
+    displayingInput();
+})
+numberOne.addEventListener('click', () => {
+    console.log("clicked")
+    firstInput = 1;
+    displayingInput();
+})
+numberOne.addEventListener('click', () => {
+    console.log("clicked")
+    firstInput = 1;
+    displayingInput();
+})
+numberOne.addEventListener('click', () => {
+    console.log("clicked")
+    firstInput = 1;
+    displayingInput();
+})
+numberOne.addEventListener('click', () => {
+    console.log("clicked")
+    firstInput = 1;
+    displayingInput();
+})
+numberOne.addEventListener('click', () => {
+    console.log("clicked")
+    firstInput = 1;
+    displayingInput();
+})
+numberOne.addEventListener('click', () => {
+    console.log("clicked")
+    firstInput = 1;
+    displayingInput();
+})
+numberOne.addEventListener('click', () => {
+    console.log("clicked")
+    firstInput = 1;
+    displayingInput();
+})
+numberOne.addEventListener('click', () => {
+    console.log("clicked")
+    firstInput = 1;
+    displayingInput();
+})
+numberOne.addEventListener('click', () => {
+    console.log("clicked")
+    firstInput = 1;
+    displayingInput();
+})
+numberOne.addEventListener('click', () => {
+    console.log("clicked")
+    firstInput = 1;
+    displayingInput();
+})
+numberOne.addEventListener('click', () => {
+    console.log("clicked")
+    firstInput = 1;
+    displayingInput();
+})
+numberOne.addEventListener('click', () => {
+    console.log("clicked")
+    firstInput = 1;
+    displayingInput();
+})
+function displayingInput(){
+    console.log("fast happening");
+const display  = document.querySelector(".display-screen");
+display.textContent = firstInput;
+};
