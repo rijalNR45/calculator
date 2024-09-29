20ab3ecd42d493c09ed0ce67547149090707fd26
rijalNR45 <rijal.nirajan45@gmail.com>
2024-09-29 16:02:15 +0545

    working in logic building

--- script.js
+++ script.js
@@ -1,34 +1,48 @@
 const inputField = document.getElementById("input-field");
 const buttonSelect = document.querySelector(".btns");
 const equalsTo = document.querySelector(".equals");
-let firstClick = null;
+let firstInput = 0;
     let secondClick = null;
     let thirdClick = null;
     let clickCount  = 0;
 buttonSelect.addEventListener('click', (e) =>{
     // console.log(e.target.innerText);
     // console.log(e);
+   
+    if(e.target.classList.contains("number")){
+        firstInput += e.target.innerText;
+        console.log(firstInput);    
+       } 
+       else if(e.target.classList.contains("operations")) {
+        secondInput = e.target.innerText;
+        console.log(secondInput);
+       }
+    
+   
+   
+    // var input = e.target.innerText;
+    // inputField.innerText += input;
+    //     clickCount++;
+    //     if(clickCount === 1){
+    //         firstClick = e.target.innerText; 
+    //         inputField.innerText = firstClick;
+            
+    //          } 
+            
+    //     else if(clickCount ===2){
 
-    var input = e.target.innerText;
-    inputField.innerText += input;
-        clickCount++;
-        if(clickCount === 1){
+    //         if(e.target.classList.contains("number")){
+    //             firstClick += e.target.innerText;
+    //         } else {
+    //             secondClick = e.target.innerText;
+    //         }
+            
+    //         console.log(secondClick);
 
-            if(e.target.classList.contains("number") ){
+    //     }   else {
+    //         thirdClick = e.target.innerText;
+    //         console.log(thirdClick);
 
-            // firstClick = e.target.innerText;
-            // console.log(firstClick);
-            // } else {
-                console.log("is a number");
-            }
-        } else if(clickCount ===2){
-            secondClick = e.target.innerText;
-            console.log(secondClick);
-
-        }   else {
-            thirdClick = e.target.innerText;
-            console.log(thirdClick);
-
-        }
+    //     }
 })
 
