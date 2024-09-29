b5a1170ccc94c73bb65a5f3146f7ace059152655
rijalNR45 <rijal.nirajan45@gmail.com>
2024-09-29 14:01:12 +0545

    add the event delegation to select multiple buttons using single function. update the input field by using the event listeners

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-const display = document.querySelector(".display-screen");
-display.textContent = 0;
-var firstInput = [0];
-const numberOne = document.getElementById("one");
-numberOne.addEventListener('click', () =>{
-    firstInput = [1];
-    display.textContent = 1;
+const inputField = document.getElementById("input-field");
+const buttonSelect = document.querySelector(".btns");
+buttonSelect.addEventListener('click', (e) =>{
+    console.log(e.target.innerText);
+    var input = e.target.innerText;
+
+    inputField.innerText += input;
+
 })
-const addBtn = document.getElementById("plus")
-addBtn.addEventListener('click',() => {
-    firstInput.reduce((acc,curr), () => {
-        return acc + curr;
-    },0)
-} )
+
