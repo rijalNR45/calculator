9c95a577aae1994232d40e4fbdeff94e77264ae1
rijalNR45 <rijal.nirajan45@gmail.com>
2024-09-29 14:32:02 +0545

    working on taking inputs properly, if two numbers are clicked simultaneously then instead of saving it in two different vars it should be stored in same var, working on that

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 const inputField = document.getElementById("input-field");
 const buttonSelect = document.querySelector(".btns");
+const equalsTo = document.querySelector(".equals");
+let firstClick = null;
+    let secondClick = null;
+    let thirdClick = null;
+    let clickCount  = 0;
 buttonSelect.addEventListener('click', (e) =>{
-    console.log(e.target.innerText);
+    // console.log(e.target.innerText);
+    // console.log(e);
+
     var input = e.target.innerText;
-
     inputField.innerText += input;
+        clickCount++;
+        if(clickCount === 1){
 
+            if(e.target.classList.contains("number") ){
+
+            // firstClick = e.target.innerText;
+            // console.log(firstClick);
+            // } else {
+                console.log("is a number");
+            }
+        } else if(clickCount ===2){
+            secondClick = e.target.innerText;
+            console.log(secondClick);
+
+        }   else {
+            thirdClick = e.target.innerText;
+            console.log(thirdClick);
+
+        }
 })
 
