5871c6d0e130bb397610ff8a61003cb80b1f2afc
rijalNR45 <rijal.nirajan45@gmail.com>
2024-10-02 12:10:01 +0545

    working on spagetti-calc

--- script.js
+++ script.js
@@ -1,114 +1,184 @@
-
-
+    let input1, operator, input2;
     const inputField = document.getElementById("input-field");
-    const buttonSelect = document.querySelector(".btns");
+    const outputField = document.getElementById("output-field");
+    const numButtons = document.querySelectorAll(".number");
     const equalsTo = document.querySelector(".equals");
+    const reset = document.getElementById("ac");
+    const del = document.getElementById("delete");
+    let inputCount = 0;
+
+    numButtons.forEach(numButton) =>  {
+        numButton.addEventListener("click", populateInputDisplay);
+    }
+
+
+    function add(num1, num2) {
+        return (num1 + num2);
+    }
+    function subtract(num1, num2) {
+        return (num1 - num2);
+        
+    }
+    function multiply(num1, num2) {
+        return (num1 * num2);
+    }
+    function divide(num1, num2) {
+        if(num2 === 0) {
+        result = 'cannot divide by zero!'            
+        } else
+        result = (num1 / num2);
+        return result;
+    }
+    function percent(num1) {
+        result = (num1 / 100);
+        return result;
+    }
+
+    function operate(input1, operator, input2) {
+        switch(operator) {
+            case '+':
+                result = add(input1, input2);
+                break;
+            case '-':
+                result = subtract(input1, input2);
+                break;
+            case '*':
+                result = multiply(input1, input2);
+                break;
+            case '/':
+                result = divide(input1, input2);
+                break;
+            default:
+                result = 'Please enter valid operator'
+            }       
+        
+    }
+    function populateInputDisplay(theTriggeredButton) {
+        (outputField.innerText) += theTriggeredButton;
+        
+   
+    }
+    function takeInputs() {
+        let firstInput = [];
+        let operation = null;
+        let secondInput = [];
+        buttonSelect.addEventListener('click', (e) => {
+            if(e.target.id === 'ac') {
+                location.reload();
+            }
+
+            firstTriggeredInput = e.target.innerText;
+            console.log(firstTriggeredInput);
+            populateDisplay(firstTriggeredInput);
+            
+
+        })
+
+    }
+    
+    
     takeInputs();
     
-    function takeInputs(){
-        let firstInput = [0];
-        let operation = null;
-        let secondInput = [0];
-        const inputField = document.getElementById("input-field");
-        var forDisplay = [0];
+    // function takeInputs(){
+    //     let firstInput = [0];
+    //     let operation = null;
+    //     let secondInput = [0];
+    //     const inputField = document.getElementById("input-field");
+    //     var forDisplay = [0];
         
-    buttonSelect.addEventListener('click', (e) =>{
-        inputField.innerText = forDisplay;
-        var updateInp;
+    // buttonSelect.addEventListener('click', (e) =>{
+    //     inputField.innerText = forDisplay;
+    //     var updateInp;
     
-       //to reload the page when AC button is clicked
-        if(e.target.id === 'ac') {
-            location.reload();
-        }        
-        if(e.target.className == "btn number"){
-            let inputsfor1 = e.target.innerText;
-            firstInput.push(inputsfor1);
-            updateInputField(firstInput);
-            // inputField.innerText += inputsfor1;
-            console.log("first input is:" + firstInput);    
-           } 
-        else if(e.target.classList.contains("operations")) {
-            operation = e.target.innerText;
-            updateInp = operation;
-            console.log(operation);
-            // inputField.innerText += inputsfor1;
+    //    //to reload the page when AC button is clicked
+    //     if(e.target.id === 'ac') {
+    //         location.reload();
+    //     }        
+    //     if(e.target.className == "btn number"){
+    //         let inputsfor1 = e.target.innerText;
+    //         firstInput.push(inputsfor1);
+    //         updateInputField(firstInput);
+    //         console.log("first input is:" + firstInput);    
+    //        } 
+    //     else if(e.target.classList.contains("operations")) {
+    //         operation = e.target.innerText;
+    //         updateInp = operation;
+    //         console.log(operation);
     
-            document.querySelectorAll(".btn.number").forEach(element => {
-                element.classList.add("secondInp");
-            });
+    //         document.querySelectorAll(".btn.number").forEach(element => {
+    //             element.classList.add("secondInp");
+    //         });
             
-           }
-        else if(e.target.classList.contains("secondInp")){
-            let inputsfor2 = e.target.innerText;
-            updateInp = inputsfor2;
-            secondInput.push(inputsfor2);
-            // inputField.innerText = inputsfor1 + inputsfor2;
+    //        }
+    //     else if(e.target.classList.contains("secondInp")){
+    //         let inputsfor2 = e.target.innerText;
+    //         updateInp = inputsfor2;
+    //         secondInput.push(inputsfor2);
     
-            console.log("second input is:" + secondInput);
-        }
+    //         console.log("second input is:" + secondInput);
+    //     }
         
     
-        else if(e.target.classList.contains("equals-to")){
-            // document.getElementById("equal").addEventListener('click', () =>{
-                var converted = convertIntoNumericValues(firstInput, secondInput);
-                var obtainedResult = operations(converted.numericInp1, converted.numericInp2, operation);
-                console.log("obtained Result is:" + obtainedResult);
-                function operations(input1, input2, operator){
-                    let result;
+    //     else if(e.target.classList.contains("equals-to")){
+    //             var converted = convertIntoNumericValues(firstInput, secondInput);
+    //             var obtainedResult = operations(converted.numericInp1, converted.numericInp2, operation);
+    //             console.log("obtained Result is:" + obtainedResult);
+    //             function operations(input1, input2, operator){
+    //                 let result;
         
-                    switch(operator){
-                        case '+':
-                            result = input1 + input2;
-                            break;
-                        case '-':
-                            result = input1 -input2;
-                            break;
-                        case '*':
-                            result = input1 * input2;
-                            break;
-                        case '/':
-                            if(input2 === 0) {
-                                result = 'lmao';
-                            } else {
-                                result = input1 / input2;
-                            }
-                            break;
-                        default:
-                            result = 'Error: invalid operator';
-                    }
-                    console.log(result);
+    //                 switch(operator){
+    //                     case '+':
+    //                         result = input1 + input2;
+    //                         break;
+    //                     case '-':
+    //                         result = input1 -input2;
+    //                         break;
+    //                     case '*':
+    //                         result = input1 * input2;
+    //                         break;
+    //                     case '/':
+    //                         if(input2 === 0) {
+    //                             result = 'lmao';
+    //                         } else {
+    //                             result = input1 / input2;
+    //                         }
+    //                         break;
+    //                     default:
+    //                         result = 'Error: invalid operator';
+    //                 }
+    //                 console.log(result);
         
-                    return result;
-                }
-       forDisplay.push(updateInp);
+    //                 return result;
+    //             }
+    //    forDisplay.push(updateInp);
     
         
-        }})
-        };
+    //     }})
+    //     };
         
         
     
     
-        function convertIntoNumericValues(a, b){
+    //     function convertIntoNumericValues(a, b){
        
-        let concatenatedInp1 = a.join('');
-        var numericInp1 = Number(concatenatedInp1);
-        console.log(numericInp1);
+    //     let concatenatedInp1 = a.join('');
+    //     var numericInp1 = Number(concatenatedInp1);
+    //     console.log(numericInp1);
     
-        let concatenatedInp2 = b.join('');
-        var numericInp2 = Number(concatenatedInp2);
-        console.log(numericInp2);
+    //     let concatenatedInp2 = b.join('');
+    //     var numericInp2 = Number(concatenatedInp2);
+    //     console.log(numericInp2);
     
-        return{numericInp1, numericInp2};
+    //     return{numericInp1, numericInp2};
     
         
-        };
-        function updateInputField(newValue) {
+    //     };
+    //     function updateInputField(newValue) {
     
-            // Append the new value to the existing innerText
-            var concatenatedValue = concat(newValue);
-            inputField.innerText += (concatenatedValue);
-        }
+    //         // Append the new value to the existing innerText
+    //         var concatenatedValue = concat(newValue);
+    //         inputField.innerText += (concatenatedValue);
+    //     }
     
         
        
